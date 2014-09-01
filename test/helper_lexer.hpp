b39d0649b61578be1a717276e5728f82f0abb93f
NoNameA 774 <nonamea774@gmail.com>
2014-09-01 18:35:45 +0900

    to_string() を実装

---
+++ test/helper_lexer.hpp
@@ -0,0 +1,8 @@
+#include "lexer.hpp"
+
+#include <string>
+
+namespace test {
+  std::string to_string(klang::TokenType t);
+  std::string to_string(klang::TokenVector const& vec);
+}  // namespace test
