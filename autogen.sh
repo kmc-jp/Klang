9208630831f73944c92af9ed05d0da59a0e3c0fa
NoNameA 774 <nonamea774@gmail.com>
2014-09-01 09:38:16 +0900

    add autogen.sh

---
+++ autogen.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+run()
+{
+    $@
+    if test $? -ne 0; then
+        echo "Failed $@"
+        exit 1
+    fi
+}
+
+run aclocal ${ACLOCAL_ARGS}
+run autoheader
+run automake --add-missing --foreign --copy
+run autoconf
