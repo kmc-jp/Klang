026c18545b39947288afebbe7c85a4143f7aa591
NoNameA 774 <nonamea774@gmail.com>
2014-09-11 00:28:34 +0900

    共通のmain 関数をまとめた。

--- test/test_lexer.cpp
+++ test/test_lexer.cpp
@@ -3,11 +3,6 @@
 #include "lexer.hpp"
 #include "helper_lexer.hpp"
 
-int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
-
 TEST(lexer, empty_source) {
   std::stringstream is;
   auto tokens = klang::tokenize(is);
