f45e6b74165fcf01640d722bace4d545361e7fd7
hatsusato <knightspm5@gmail.com>
2014-09-17 03:40:23 +0900

    Add parse_add_assign_expression() member

--- src/parser.cpp
+++ src/parser.cpp
@@ -345,6 +345,20 @@ ast::AssignExpressionPtr Parser::parse_single_assign_expression() {
   return nullptr;
 }
 
+ast::AssignExpressionPtr Parser::parse_add_assign_expression() {
+  const auto snapshot = make_snapshot();
+  if (auto lhs_expression = parse_or_expression()) {
+    if (parse_symbol(":+=")) {
+      if (auto rhs_expression = parse_or_expression()) {
+        return make_unique<ast::AddAssignExpressionData>(
+            std::move(lhs_expression), std::move(rhs_expression));
+      }
+    }
+  }
+  rewind(snapshot);
+  return nullptr;
+}
+
 TokenType Parser::current_type() const {
   return is_eof() ? TokenType::IGNORE : current_->type();
 }
