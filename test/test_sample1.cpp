05a38711b75b6b3ca7eee84d5b7fbce2d863966f
NoNameA 774 <nonamea774@gmail.com>
2014-09-16 10:07:09 +0900

    _ をテストの名前に入れるのは非推奨とされていたので取り除いた。

--- test/test_sample1.cpp
+++ test/test_sample1.cpp
@@ -2,14 +2,14 @@
 
 // http://opencv.jp/googletestdocs/primer.html Google test 入門ガイド
 
-TEST(alwaysPass, assert_true) {
+TEST(alwaysPass, assertTrue) {
   ASSERT_TRUE(true);
 }
 
-TEST(alwaysPass, eq1_1) {
+TEST(alwaysPass, eq11) {
   EXPECT_EQ(1, 1);
 }
 
-TEST(alwaysPass, lt1_2) {
+TEST(alwaysPass, lt12) {
   EXPECT_LT(1, 2);
 }
