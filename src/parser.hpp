4fbc6f87499ace83bf2fbd41c7b632d8b311cf44
hatsusato <knightspm5@gmail.com>
2014-09-17 01:03:55 +0900

    Add parse_translation_unit() member
    Add also parse_function_definition() member declaration.

--- src/parser.hpp
+++ src/parser.hpp
@@ -15,6 +15,8 @@ class Parser {
   ast::IntegerLiteralPtr parse_integer_literal();
   ast::CharacterLiteralPtr parse_character_literal();
   ast::StringLiteralPtr parse_string_literal();
+  ast::TranslationUnitPtr parse_translation_unit();
+  ast::FunctionDefinitionPtr parse_function_definition();
  private:
   TokenType current_type() const;
   std::string current_string() const;
