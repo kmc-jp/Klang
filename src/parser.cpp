adfc5a4dff90c4a331bd17a9116f59a9e994d2dc
hatsusato <knightspm5@gmail.com>
2014-09-17 03:41:25 +0900

    Add parse_subtract_assign_expression() member

--- src/parser.cpp
+++ src/parser.cpp
@@ -359,6 +359,20 @@ ast::AssignExpressionPtr Parser::parse_add_assign_expression() {
   return nullptr;
 }
 
+ast::AssignExpressionPtr Parser::parse_subtract_assign_expression() {
+  const auto snapshot = make_snapshot();
+  if (auto lhs_expression = parse_or_expression()) {
+    if (parse_symbol(":-=")) {
+      if (auto rhs_expression = parse_or_expression()) {
+        return make_unique<ast::SubtractAssignExpressionData>(
+            std::move(lhs_expression), std::move(rhs_expression));
+      }
+    }
+  }
+  rewind(snapshot);
+  return nullptr;
+}
+
 TokenType Parser::current_type() const {
   return is_eof() ? TokenType::IGNORE : current_->type();
 }
