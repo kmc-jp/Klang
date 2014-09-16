990c0736f057d74e577ac3b32e2878943687bf34
hatsusato <knightspm5@gmail.com>
2014-09-17 04:03:55 +0900

    Add parse_less_or_equal_expression() member

--- src/parser.cpp
+++ src/parser.cpp
@@ -523,6 +523,20 @@ ast::ComparativeExpressionPtr Parser::parse_greater_expression() {
   return nullptr;
 }
 
+ast::ComparativeExpressionPtr Parser::parse_less_or_equal_expression() {
+  const auto snapshot = make_snapshot();
+  if (auto lhs_expression = parse_additive_expression()) {
+    if (parse_symbol("<=")) {
+      if (auto rhs_expression = parse_additive_expression()) {
+        return make_unique<ast::LessOrEqualExpressionData>(
+            std::move(lhs_expression), std::move(rhs_expression));
+      }
+    }
+  }
+  rewind(snapshot);
+  return nullptr;
+}
+
 TokenType Parser::current_type() const {
   return is_eof() ? TokenType::IGNORE : current_->type();
 }
