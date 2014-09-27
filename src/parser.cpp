b6b1770da7b6c0731d6bd08fec97b34d7a121cc7
hatsusato <knightspm5@gmail.com>
2014-09-27 22:09:09 +0900

    Add ctor of Parser

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,4 +1,10 @@
 #include "parser.hpp"
 
 namespace klang {
+
+Parser::Parser(TokenVector tokens)
+    : tokens_(std::move(tokens)),
+      current_(std::begin(tokens_))
+{}
+
 }  // namespace klang
--- src/parser.hpp
+++ src/parser.hpp
@@ -1,9 +1,17 @@
 #ifndef KMC_KLANG_PARSER_HPP
 #define KMC_KLANG_PARSER_HPP
 
+#include "lexer.hpp"
+
 namespace klang {
 
 class Parser {
+ public:
+  Parser(TokenVector tokens);
+ private:
+  using Pointer = TokenVector::const_iterator;
+  const TokenVector tokens_;
+  Pointer current_;
 };
 
 }  // namespace klang
