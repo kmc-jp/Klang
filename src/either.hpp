6e2c95f09d2f638b2ef47aef5d1f06090d8ed406
hatsusato <knightspm5@gmail.com>
2014-10-04 21:54:58 +0900

    Add implementations for Either

--- src/either.hpp
+++ src/either.hpp
@@ -14,40 +14,100 @@ struct RightTag {} right_tag;
 template <typename Left, typename Right>
 class Either {
  public:
-  Either(const Right& right);
-  Either(Right&& right);
-  Either(LeftTag, const Left& left);
-  Either(LeftTag, Left&& left);
-  Either(RightTag , const Right& right);
-  Either(RightTag, Right&& right);
-  Either(const Either& either);
-  Either(Either&& either);
-  Either& operator=(const Either& either);
-  Either& operator=(Either&& either);
-  void swap(Either& that);
-  bool is_left() const;
-  bool is_right() const;
-  const Left& left_value() const;
-  const Right& right_value() const;
-  explicit operator bool() const;
-  const Right& operator*() const&;
-  Right& operator*() &;
-  Right&& operator*() &&;
-  const Right* operator->() const;
-  Right* operator->();
+  Either(const Right& right)
+      : Either(right_tag, right)
+  {}
+  Either(Right&& right)
+      : Either(right_tag, std::move(right))
+  {}
+  Either(LeftTag, const Left& left)
+      : left_(left), right_()
+  {}
+  Either(LeftTag, Left&& left)
+      : left_(std::move(left)), right_()
+  {}
+  Either(RightTag, const Right& right)
+      : left_(), right_(right)
+  {}
+  Either(RightTag, Right&& right)
+      : left_(), right_(std::move(right))
+  {}
+  Either(const Either& either) = default;
+  Either(Either&& either) = default;
+  Either& operator=(const Either& either) = default;
+  Either& operator=(Either&& either) = default;
+  void swap(Either& that) {
+    Either tmp = std::move(*this);
+    *this = std::move(that);
+    that = std::move(tmp);
+  }
+  bool is_left() const {
+    return static_cast<bool>(left_);
+  }
+  bool is_right() const {
+    return static_cast<bool>(right_);
+  }
+  const Left& left_value() const {
+    return *left_;
+  }
+  const Right& right_value() const {
+    return *right_;
+  }
+  explicit operator bool() const {
+    return is_right();
+  }
+  const Right& operator*() const& {
+    return *right_;
+  }
+  Right& operator*() & {
+    return *right_;
+  }
+  Right&& operator*() && {
+    return std::move(*right_);
+  }
+  const Right* operator->() const {
+    return &*right_;
+  }
+  Right* operator->() {
+    return &*right_;
+  }
  private:
   optional<Left> left_;
   optional<Right> right_;
 };
 
 template <typename Left, typename Right, typename E = Either<Left, Right> >
-bool operator==(const E& lhs, const E& rhs);
+bool operator==(const E& lhs, const E& rhs) {
+  if (lhs && rhs) {
+    return *lhs == *rhs;
+  } else if (!lhs && !rhs) {
+    return lhs.left_value() == rhs.left_value();
+  } else {
+    return false;
+  }
+}
 
 template <typename Left, typename Right, typename E = Either<Left, Right> >
-bool operator<(const E& lhs, const E& rhs);
+bool operator<(const E& lhs, const E& rhs) {
+  if (lhs) {
+    if (rhs) {
+      return *lhs < *rhs;
+    } else {
+      return false;
+    }
+  } else {
+    if (rhs) {
+      return true;
+    } else {
+      return lhs.left_value() < rhs.left_value();
+    }
+  }
+}
 
 template <typename Left, typename Right, typename E = Either<Left, Right> >
-void swap(E& lhs, E& rhs);
+void swap(E& lhs, E& rhs) {
+  lhs.swap(rhs);
+}
 
 }  // namespace klang
 
