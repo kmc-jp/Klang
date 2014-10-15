5f1a1c5c0bac6368e4d7f8154e44eb5de197a9a8
hatsusato <knightspm5@gmail.com>
2014-10-15 15:21:31 +0900

    Use union instead of optional
    Implement special member functions which handle union.

--- src/either.hpp
+++ src/either.hpp
@@ -1,12 +1,7 @@
 #ifndef KMC_KLANG_EITHER_HPP
 #define KMC_KLANG_EITHER_HPP
 
-#include <boost/optional.hpp>
-
 namespace klang {
-
-template <typename T>
-using optional = boost::optional<T>;
 
 struct LeftTag {};
 struct RightTag {};
@@ -14,16 +9,82 @@ struct RightTag {};
 constexpr LeftTag left_tag;
 constexpr RightTag right_tag;
 
-template <typename Left, typename Right>
+template <typename L, typename R>
 class Either {
  public:
-  Either(const Either& either) = default;
-  Either(Either&& either) = default;
-  Either& operator=(const Either& either) = default;
-  Either& operator=(Either&& either) = default;
+  Either(const Either& that)
+      : is_right_{that.is_right_} {
+    construct(that);
+  }
+  Either(Either&& that)
+      : is_right_{that.is_right_} {
+    construct(std::move(that));
+  }
+  Either& operator=(const Either& that) {
+    assign(that);
+    return *this;
+  }
+  Either& operator=(Either&& that) {
+    assign(std::move(that));
+    return *this;
+  }
+  ~Either() {
+    destruct();
+  }
  private:
-  optional<Left> left_;
-  optional<Right> right_;
+  void construct(const Either& src) {
+    if (src.is_right_) {
+      new (&right_) R{src.right_};
+    } else {
+      new (&left_) L{src.left_};
+    }
+  }
+  void construct(Either&& src) {
+    if (src.is_right_) {
+      new (&right_) R{std::move(src.right_)};
+    } else {
+      new (&left_) L{std::move(src.left_)};
+    }
+  }
+  void assign(const Either& src) {
+    if (is_right_ == src.is_right_) {
+      if (is_right_) {
+        right_ = src.right_;
+      } else {
+        left_ = src.left_;
+      }
+    } else {
+      destruct();
+      construct(src);
+      is_right_ = src.is_right_;
+    }
+  }
+  void assign(Either&& src) {
+    if (is_right_ == src.is_right_) {
+      if (is_right_) {
+        right_ = std::move(src.right_);
+      } else {
+        left_ = std::move(src.left_);
+      }
+    } else {
+      destruct();
+      construct(std::move(src));
+      is_right_ = src.is_right_;
+    }
+  }
+  void destruct() {
+    if (is_right_) {
+      right_.~R();
+    } else {
+      left_.~L();
+    }
+  }
+ private:
+  bool is_right_;
+  union {
+    L left_;
+    R right_;
+  };
 };
 
 }  // namespace klang
