d7314e2c0c44fa433d3adb82c352cc22edc215c0
NoNameA 774 <nonamea774@gmail.com>
2014-10-20 13:41:00 +0900

    コマンドラインのサンプル的な

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
-#include "codegen.hpp"
+#include <llvm/Support/CommandLine.h>
+
+#include <iostream>
+#include <string>
+
+#include "config.h"
+
+namespace /* unnamed namespace */ {
+  using namespace llvm::cl;
+  llvm::cl::opt<std::string> OutputFile("o", desc("Optput file name"),
+                                        value_desc("filename"), init("a.out"));
+} // unnamed namespace
 
 int main(int argc, char** argv) {
-  klang::codegen::test();
+  SetVersionPrinter([](){ std::cout << PACKAGE_STRING << std::endl; });
+  llvm::cl::ParseCommandLineOptions(argc, argv, "CL");
+
+  std::cout << OutputFile << std::endl;
+
   return 0;
 }
