c861222e9a253ac0c92ec445b0fd8b98a3965841
NoNameA 774 <nonamea774@gmail.com>
2014-10-20 12:51:53 +0900

    main いじってみる

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,29 @@
 #include "codegen.hpp"
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/IRBuilder.h>
+#include "llvm/Support/Debug.h"
+#include "llvm/Support/FormattedStream.h"
+#include "llvm/Support/PrettyStackTrace.h"
+#include "llvm/Support/Signals.h"
+#include "llvm/Support/TargetSelect.h"
 
 int main(int argc, char** argv) {
-  klang::codegen::test();
+  // llvm::InitializeNativeTarget();
+  llvm::sys::PrintStackTraceOnErrorSignal();
+  llvm::PrettyStackTraceProgram X(argc, argv);
+  llvm::EnableDebugBuffering = true;
+
+  llvm::LLVMContext& context = llvm::getGlobalContext();
+  llvm::Module* module = new llvm::Module("top", context);
+  llvm::IRBuilder<> builder(context);
+
+  llvm::FunctionType* funcType =
+      llvm::FunctionType::get(builder.getInt32Ty(), false);
+  llvm::Function* mainFunc =
+      llvm::Function::Create(funcType, llvm::Function::ExternalLinkage, "main", module);
+
+  module->dump();
+  // klang::codegen::test();
   return 0;
 }
